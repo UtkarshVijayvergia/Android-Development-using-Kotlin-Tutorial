64750b3157cf264b42bddcaafb9bdb55fc64975d
Utkarsh <utkarsh.v1901@gmail.com>
2023-02-25 18:10:39 +0530

    Backend ready click-counter

--- ClickCounter/app/src/main/java/com/example/click_counter/MainActivity.kt
+++ ClickCounter/app/src/main/java/com/example/click_counter/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.click_counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter = 0
+        val buttonClicker: Button = findViewById(R.id.button1)
+        val textChanger: TextView = findViewById(R.id.textView)
+
+        buttonClicker.setOnClickListener{
+            counter++
+            textChanger.text = counter.toString()
+        }
     }
 }
